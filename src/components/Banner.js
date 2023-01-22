05d5ec3fc49efa4a8d47e8b2e4c904c0b9b97f7c
Łukasz <lukasz.poczta12@gmail.com>
2023-01-22 15:11:05 +0100

    banner buttons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { feedBanner } from '../utils/data';
 import { urlFor, client } from '../utils/client';
 import classes from './Banner.module.css';
+import { GrNext, GrPrevious } from 'react-icons/gr';
 
 const Banner = () => {
 	const [banner, setBanner] = useState(null);
@@ -9,19 +10,44 @@ const Banner = () => {
 
 	useEffect(() => {
 		client.fetch(feedBanner).then((data) => setBanner(data));
-	}, [feedBanner]);
+	}, []);
+
 	useEffect(() => {
+		
 		const interval = setInterval(() => {
 			setCurrentIndex((currentIndex + 1) % banner.length);
+			
 		}, 10000);
+
 		return () => clearInterval(interval);
 	}, [currentIndex, banner]);
+
+	const nextBannerHandler = () => {
+		setCurrentIndex((currentIndex + 1) % banner.length);
+	};
+	const prevBannerHandler = () => {
+		if (currentIndex === 0) {
+			setCurrentIndex(banner.length - 1);
+		} else {
+			setCurrentIndex((currentIndex - 1) % banner.length);
+		}
+	};
 
 	return (
 		<>
 			{banner && (
 				<div className={classes.container}>
+					<button className={classes.prevBtn}>
+						<GrPrevious
+							onClick={prevBannerHandler}
+							className={classes.btnIcon}
+						/>
+					</button>
+					<button className={classes.nextBtn}>
+						<GrNext onClick={nextBannerHandler} className={classes.btnIcon} />
+					</button>
 					{banner.map((banner, index) => (
+						
 						<div
 							className={`${classes.bannerBox} ${
 								index === currentIndex ? classes.active : classes.inactive
