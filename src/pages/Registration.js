3ff495ea85ac5b0ec46a2f1e57c724ec593305dd
Łukasz <lukasz.poczta12@gmail.com>
2023-01-29 14:34:31 +0100

    added register form validation

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,19 +1,116 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 
-import classes from "../styles/Registration.module.css"
+import classes from '../styles/Registration.module.css';
 const Registration = () => {
-  const [error, setError] = useState(false);
+	const [error, setError] = useState(false);
 
-  const erorrHandler = () => {
+	const [name, setName] = useState('');
+	const [nameError, setNamError] = useState(false);
+
+	const [email, setEmail] = useState('');
+	const [emailError, setEmailError] = useState(false);
+
+	const [password, setPassword] = useState('');
+	const [passwordError, setPasswordError] = useState(false);
+
+	const [repeatedPassword, setRepeatedPassword] = useState('');
+	const [repeatedpasswordError, setRepeatedPasswordError] = useState(false);
+
+	const [isChecked, setIsChecked] = useState(false);
+	const [errorIsChecked, setErrorIsChecked] = useState(false);
+
+	const nameHandler = (e) => {
+		setName(e.target.value);
+	};
+	const emailHandler = (e) => {
+		setEmail(e.target.value);
+	};
+	const passwordHandler = (e) => {
+		setPassword(e.target.value);
+	};
+	const repeatedPasswordHandler = (e) => {
+		setRepeatedPassword(e.target.value);
+	};
+
+	const erorrHandler = () => {
 		setError(false);
 	};
 
+	const onRegistrationHandler = (e) => {
+		e.preventDefault();
+		if (isNameValid()) {
+			setNamError(false);
+		} else {
+			setNamError(true);
+			setError(true);
+		}
+		if (isEmailValid()) {
+			setEmailError(false);
+		} else {
+			setEmailError(true);
+			setError(true);
+		}
+		if (isPasswordValid()) {
+			setPasswordError(false);
+		} else {
+			setPasswordError(true);
+			setError(true);
+		}
+		if (isRepeatPasswordValid()) {
+			setRepeatedPasswordError(false);
+		} else {
+			setRepeatedPasswordError(true);
+			setError(true);
+		}
+		if (isChecked) {
+			setErrorIsChecked(false);
+		} else {
+			setErrorIsChecked(true);
+			setError(true);
+		}
+	};
 
+	const isNameValid = () => {
+		let isValid = true;
+		if (name === '') {
+			isValid = false;
+		}
+		return isValid;
+	};
+	const isEmailValid = () => {
+		let isValid = true;
+		const re =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if (!re.test(email)) {
+			isValid = false;
+		} else {
+		}
+		return isValid;
+	};
+	const isPasswordValid = () => {
+		let isValid = true;
+		if (password.length < 8) {
+			isValid = false;
+		}
+		if (!/\d/.test(password)) {
+			isValid = false;
+		}
 
-
-
-
+		if (!/[!@#$%^&*()+=._-]/.test(password)) {
+			isValid = false;
+		} else {
+		}
+		return isValid;
+	};
+	const isRepeatPasswordValid = () => {
+		let isValid = true;
+		if (password !== repeatedPassword) {
+			setRepeatedPasswordError(true);
+			isValid = false;
+		}
+		return isValid;
+	};
 
 	return (
 		<div className={classes.mainContainer}>
@@ -23,39 +120,99 @@ const Registration = () => {
 					<p>The registration form contains errors.</p>
 				</div>
 			)}
-      <form className={classes.registerContainer}>
+			<form
+				className={classes.registerContainer}
+				onSubmit={onRegistrationHandler}>
+				<h2>Registration</h2>
 
-      <h2>Registration</h2>
-				<div className={classes.nameBox}>
+				<div
+					className={
+						nameError
+							? `${classes.nameBox} ${classes.inputError}`
+							: classes.nameBox
+					}>
 					<label for='name'>* Your name:</label>
-					<input name='name' id='name'></input>
+					<input
+						name='name'
+						id='name'
+						value={name}
+						onChange={nameHandler}></input>
+					{nameError && <p>Please write your name</p>}
 				</div>
-				<div className={classes.emailBox}>
+
+				<div
+					className={
+						emailError
+							? `${classes.emailBox} ${classes.inputError}`
+							: classes.emailBox
+					}>
 					<label for='email'>* E-mail:</label>
-					<input name='email' id='email' value=''></input>
+					<input
+						name='email'
+						id='email'
+						value={email}
+						onChange={emailHandler}
+						autoComplete='email'></input>
+					{emailError && <p>Invalid email address format</p>}
 				</div>
-				<div className={classes.passwordBox}>
+				<div
+					className={
+						passwordError
+							? `${classes.passwordBox} ${classes.inputError}`
+							: classes.passwordBox
+					}>
 					<label for='password'>* Password:</label>
-					<input name='password' id='password'></input>
+					<input
+						name='password'
+						id='password'
+						type='password'
+						value={password}
+						onChange={passwordHandler}
+						autocomplete='new-password'></input>
+					{passwordError && (
+						<p>The password must meet the described conditions</p>
+					)}
 				</div>
-				<div className={classes.passwordBox}>
+				<div
+					className={
+						repeatedpasswordError
+							? `${classes.passwordBox} ${classes.inputError}`
+							: classes.passwordBox
+					}>
 					<label for='repeatPassword'>* Repeat password:</label>
-					<input name='repeatPassword' id='repeatPassword'></input>
+					<input
+						name='repeatPassword'
+						id='repeatPassword'
+						type='password'
+						value={repeatedPassword}
+						onChange={repeatedPasswordHandler}
+						autocomplete='new-password'
+					/>
+					{repeatedpasswordError && <p>The passwords are different</p>}
 				</div>
 				<p>Additional information:</p>
 				<div className={classes.checkBox}>
-					<input type='checkbox'></input>
-					<label>
+					<input
+						type='checkbox'
+						id='check'
+						checked={isChecked}
+						onChange={() => setIsChecked(!isChecked)}
+
+						// className={classes.errorCheckBox}
+					/>
+					<label for='check'>
 						I have read the regulations of the online store and accept its
 						content.
 					</label>
+					{errorIsChecked && <p>You have to accept the terms</p>}
 				</div>
 				<div>
-					<button>Register</button>
+					<button type='submit'>Register</button>
 				</div>
-        <div className={classes.moreInfoBox}>
-          * Password should to contain min 8 characters one digital and one special character 
-        </div>
+				<div className={classes.moreInfoBox}>
+					* Password should to contain min 8 characters one digital and one
+					special character
+				</div>
 			</form>
 		</div>
 	);
