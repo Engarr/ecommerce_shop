01db571dc17e860bba2506b46f930edd2553089e
Łukasz <lukasz.poczta12@gmail.com>
2023-02-20 12:32:38 +0100

    changed login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlineClose } from 'react-icons/ai';
 import bcrypt from 'bcryptjs';
@@ -10,59 +10,105 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 
 const Login = () => {
-	const [error, setError] = useState(false);
-	const [password, setPassword] = useState('');
-	const [email, setEmail] = useState('');
 	const { setUserData, setIsLogin } = useStateContext();
 	const navigate = useNavigate();
-	const passwordHandler = (e) => {
-		setPassword(e.target.value);
-	};
-	const emailHandler = (e) => {
-		setEmail(e.target.value);
-	};
 
 	const erorrHandler = () => {
-		setError(false);
+		setErrors((prevError) => ({
+			...prevError,
+			error: false,
+		}));
 	};
+
+	const [errors, setErrors] = useState({
+		error: false,
+		email: false,
+		password: false,
+	});
+	const [formData, setFormData] = useState({
+		password: '',
+		email: '',
+	});
+	const formDataHandler = (e) => {
+		setFormData((prevData) => ({
+			...prevData,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
 	const isPasswordValid = () => {
 		let isValid = true;
-		if (password.length < 8) {
+		if (formData.password.length < 8) {
 			isValid = false;
 		}
-		if (!/\d/.test(password)) {
+		if (!/\d/.test(formData.password)) {
 			isValid = false;
 		}
 
-		if (!/[!@#$%^&*()+=._-]/.test(password)) {
+		if (!/[!@#$%^&*()+=._-]/.test(formData.password)) {
 			isValid = false;
 		}
 		return isValid;
 	};
+
 	const isEmailValid = () => {
 		let isValid = true;
 		const re =
 			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		if (!re.test(email)) {
+		if (!re.test(formData.email)) {
 			isValid = false;
-		} else {
+		}
+		return isValid;
+	};
+	const validation = () => {
+		let isValid = true;
+		if (!isEmailValid()) {
+			isValid = false;
+			setErrors((prevError) => ({
+				...prevError,
+				email: true,
+			}));
+		}
+		if (!isPasswordValid()) {
+			isValid = false;
+			setErrors((prevError) => ({
+				...prevError,
+				password: true,
+			}));
 		}
 		return isValid;
 	};
 
+	useEffect(() => {
+		if (formData.email !== '') {
+			setErrors((prevError) => ({
+				...prevError,
+				email: false,
+			}));
+		}
+		if (formData.password !== '') {
+			setErrors((prevError) => ({
+				...prevError,
+				password: false,
+			}));
+		}
+	}, [formData]);
+
 	const onLoginHandler = async (e) => {
 		e.preventDefault();
 
-		if (isPasswordValid() && isEmailValid()) {
+		if (validation()) {
 			try {
-				const query = loginUser(email);
+				const query = loginUser(formData.email);
 				const response = await client.fetch(query);
 				const data = response[0].password;
 
-				const match = await bcrypt.compare(password, data);
+				const match = await bcrypt.compare(formData.password, data);
 				if (!match) {
-					setError(true);
-					setPassword('');
+					setErrors((prevError) => ({
+						...prevError,
+						error: true,
+					}));
 				}
 				if (match) {
 					setUserData({
@@ -70,8 +116,10 @@ const Login = () => {
 						email: response[0].email,
 					});
 					setIsLogin(true);
-					setEmail('');
-					setPassword('');
+					setFormData({
+						password: '',
+						email: '',
+					});
 
 					toast.success(
 						`You have successfully logged in. Welcome ${response[0].name}!`
@@ -79,16 +127,22 @@ const Login = () => {
 					navigate('/home');
 				}
 			} catch {
-				setError(true);
+				setErrors((prevError) => ({
+					...prevError,
+					error: true,
+				}));
 			}
 		} else {
-			setError(true);
+			setErrors((prevError) => ({
+				...prevError,
+				error: true,
+			}));
 		}
 	};
 
 	return (
 		<div className={classes.mainContainer}>
-			{error && (
+			{errors.error && (
 				<div className={classes.error}>
 					<AiOutlineClose className={classes.closeBtn} onClick={erorrHandler} />
 					<p>Invalid login details.</p>
@@ -98,15 +152,16 @@ const Login = () => {
 			<form className={classes.loginContainer} onSubmit={onLoginHandler}>
 				<h2>LOGIN</h2>
 				<div className={classes.emailBox}>
-					<label htmlFor='e-mail'> E-mail:</label>
+					<label htmlFor='email'> E-mail:</label>
 
 					<input
-						name='e-mail'
-						id='e-mail'
-						onChange={emailHandler}
-						value={email}
+						name='email'
+						id='email'
+						onChange={formDataHandler}
+						value={formData.email}
 						autoComplete='off'
-						className={error ? classes.inputError : ''}></input>
+						className={errors.email ? classes.inputError : ''}></input>
+					{errors.email && <p>Please enter your email.</p>}
 				</div>
 				<div className={classes.passwordBox}>
 					<label htmlFor='password'> Password:</label>
@@ -115,10 +170,11 @@ const Login = () => {
 						type='password'
 						name='password'
 						id='password'
-						onChange={passwordHandler}
-						value={password}
+						onChange={formDataHandler}
+						value={formData.password}
 						autoComplete='off'
-						className={error ? classes.inputError : ''}></input>
+						className={errors.password ? classes.inputError : ''}></input>
+					{errors.password && <p>Please enter your password.</p>}
 				</div>
 				<button type='submit'>Login</button>
 				<div className={classes.createAccountBox}>
