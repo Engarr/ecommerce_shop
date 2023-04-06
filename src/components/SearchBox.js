4aa9f3b6c62357057f082173e557e2f17a43115b
Łukasz <lukasz.poczta12@gmail.com>
2023-04-06 12:35:46 +0200

    changed searchBox component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,8 +6,8 @@ import Modal from './Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../store/ui-slice';
 import classes from './SearchBox.module.css';
-import { client } from '../utils/client';
 import Product from './Product';
+import Spinner from '../components/Spinner';
 
 const SearchBox = () => {
 	const isSearchVisible = useSelector((state) => state.ui.isSearchVisible);
@@ -17,24 +17,41 @@ const SearchBox = () => {
 	};
 
 	const [value, setValue] = useState('');
-	const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState('');
+	const [isProducts, setIsProducts] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const valueHandler = async (e) => {
 		setValue(e.target.value);
 	};
-
 	const searchHandler = async () => {
 		if (value !== '') {
-			const query = `*[ _type == "product" && name match "^${value.toLowerCase().trim()}*" ]`;
-			console.log(query);
-			const data = await client.fetch(query);
-			console.log(data);
-			setProducts(data);
+			setIsLoading(true);
+			const response = await fetch(
+				`http://localhost:8080/feed/search/${value}`
+			);
+			const data = await response.json();
+			const products = data.products;
+			if (response.ok) {
+				const prefix = `http://localhost:8080/`;
+				const newArr = products.map((element) => ({
+					...element,
+					imageUrl: prefix + element.imageUrl[0],
+				}));
+				setProducts(newArr);
+				setIsProducts(true);
+
+				if (products.length === 0) {
+					setIsProducts(false);
+				}
+				setIsLoading(false);
+			} else {
+				console.log('error');
+			}
 		} else {
 			setProducts([]);
 		}
 	};
-
 	useEffect(() => {
 		searchHandler();
 		// eslint-disable-next-line
@@ -56,11 +73,25 @@ const SearchBox = () => {
 						/>
 					</div>
 				</div>
+
 				<div className={classes.productsContainer}>
-					{products.length > 0 &&
-						products.map((product) => (
-							<Product key={product._id} product={product}  />
-						))}
+					{isLoading ? (
+						<div className={classes.SpinerBox}>
+							<Spinner message='Loading...' />
+						</div>
+					) : (
+						<div className={classes.productContainer}>
+							{products.length > 0 &&
+								products.map((product) => (
+									<Product key={product._id} product={product} />
+								))}
+							{!isProducts && (
+								<div className={classes.searchError}>
+									<p>Unfortunately, no products could be found</p>
+								</div>
+							)}
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
